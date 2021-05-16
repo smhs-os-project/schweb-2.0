6ebfa9885e34f0ded07cc9fc4d257f7753e6fbd4
Yi-Jyun Pan <pan93412@gmail.com>
2021-05-17 02:37:37 +0800

    feat(tailwind): add the Schweb color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,26 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      "sweet-orange": {
+        DEFAULT: "#EB8A6C",
+      },
+      "hot-red": {
+        DEFAULT: "#BF3D3D",
+      },
+      "sky-blue": {
+        DEFAULT: "#6CAEEB",
+      },
+      "night-blue": {
+        DEFAULT: "#6A89D7",
+      },
+      "grass-green": {
+        DEFAULT: "#58AF6C",
+      },
+      "cold-purple": {
+        DEFAULT: "#9796C8",
+      },
+    },
     extend: {
     },
   },
